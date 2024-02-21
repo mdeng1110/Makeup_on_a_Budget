af6d15831f798856a0944b25a885558653f3b77a
mdeng1110 <maydeng1110@yahoo.com>
2024-02-20 16:24:35 -0800

    updated form

--- app/views.py
+++ app/views.py
@@ -3,8 +3,5 @@ from .forms import UserInputForm
 
 
 def index(request):
-    return render(request, 'index.html', {})
-
-def get_user_input(request):
     form = UserInputForm()
     return render(request, "index.html", {"form": form})
